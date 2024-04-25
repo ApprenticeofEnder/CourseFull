9437f39dbc56cc56b465ddf6b5b3f2fe9dc18d56
Robert Babaev <github@robertbabaev.tech>
2024-04-25 00:17:30 -0400

    get the project up and running

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 export default function Navbar(){
     return <nav>
-        
+        <h1>CourseFull</h1>
     </nav>
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1 className="italic">Hello!</h1>
     </div>
   );
