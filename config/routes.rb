695cc57f0da374c08be6a9f8b55502e14a3c84db
Robert Babaev <github@robertbabaev.tech>
2024-04-26 00:00:38 -0400

    fix routing BS

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :deliverables
-  resources :semesters
-  resources :courses
+  scope '/api/v1' do
+    resources :deliverables
+    resources :semesters
+    resources :courses
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
