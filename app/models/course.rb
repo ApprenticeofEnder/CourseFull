b8aaca1998c32e635a06087d0e6947e11b9cb0cc
Robert Babaev <github@robertbabaev.tech>
2024-04-26 03:28:19 -0400

    add some unit testing (need to fiddle with supabase)

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+    validates :title, presence: true
+    validates :course_code, presence: true
 end
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Can't create course without course code" do
+    course = Course.new
+    assert_not course.save
+  end
 end
