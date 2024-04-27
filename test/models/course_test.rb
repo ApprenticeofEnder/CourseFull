1027f34eb17373e9ace1c4fc349c324eb3e8dea2
Robert Babaev <github@robertbabaev.tech>
2024-04-27 19:51:33 -0400

    convert to rspec for tests

--- test/models/course_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class CourseTest < ActiveSupport::TestCase
-  test "Can't create course without course code" do
-    course = Course.new
-    assert_not course.save
-  end
-end
