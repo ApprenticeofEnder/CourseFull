01bf2da9afd88bd4779b380964ad6e2a7d9c8690
Robert Babaev <github@robertbabaev.tech>
2024-04-30 03:03:15 -0400

    add possible reverse proxy

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    // Miiiight need this later? not sure
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/api/v1/:endpoint*',
+    //             destination: 'http://localhost:8080/api/v1/:endpoint*',
+    //         },
+    //     ];
+    // },
 };
 
 export default nextConfig;
