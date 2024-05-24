4886982e848d75cc0a2b7595e96e4627383f6718
Robert Babaev <github@robertbabaev.tech>
2024-05-24 00:09:28 -0400

    cleanup

--- client/components/Modal/CreateCourse.tsx
+++ client/components/Modal/CreateCourse.tsx
@@ -11,9 +11,9 @@ import { Listbox, Transition } from '@headlessui/react';
 import Button from '@/components/Button/Button';
 import ConfirmButton from '@/components/Button/ConfirmButton';
 import DisclosureButton from '@/components/Button/DisclosureButton';
-import { Endpoints, ItemStatus } from '@/lib/enums';
-import { ReadableStatus, courseURL } from '@/lib/helpers';
-import { Course, SessionProps } from '@/lib/types';
+import { ItemStatus } from '@/lib/enums';
+import { ReadableStatus } from '@/lib/helpers';
+import { SessionProps } from '@/lib/types';
 import { createCourse } from '@/services/courseService';
 
 interface CourseModalProps extends SessionProps {
--- client/components/Modal/CreateSemester.tsx
+++ client/components/Modal/CreateSemester.tsx
@@ -7,15 +7,14 @@ import {
     ModalFooter,
     Input,
 } from '@nextui-org/react';
-import { useRouter } from 'next/navigation';
 import { Listbox, Transition } from '@headlessui/react';
 
 import Button from '@/components/Button/Button';
 import ConfirmButton from '@/components/Button/ConfirmButton';
 import DisclosureButton from '@/components/Button/DisclosureButton';
-import { Endpoints, ItemStatus } from '@/lib/enums';
-import { ReadableStatus, semesterURL } from '@/lib/helpers';
-import { Semester, SessionProps } from '@/lib/types';
+import { ItemStatus } from '@/lib/enums';
+import { ReadableStatus } from '@/lib/helpers';
+import { SessionProps } from '@/lib/types';
 import { createSemester } from '@/services/semesterService';
 
 export default function CreateSemesterModal({ session }: SessionProps) {
