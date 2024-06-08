c19a89fc81b7abf208987d4566234de3e6ad55e5
Robert Babaev <github@robertbabaev.tech>
2024-06-07 22:27:00 -0400

    remove unnecessary imports

--- client/components/Modal/CreateDeliverable.tsx
+++ client/components/Modal/CreateDeliverable.tsx
@@ -4,16 +4,11 @@ import {
     ModalHeader,
     ModalBody,
     ModalFooter,
-    Input,
-    Textarea,
 } from '@nextui-org/react';
-import { Listbox, Transition } from '@headlessui/react';
 
 import Button from '@/components/Button/Button';
 import ConfirmButton from '@/components/Button/ConfirmButton';
-import DisclosureButton from '@/components/Button/DisclosureButton';
 import { ItemStatus } from '@/lib/enums';
-import { ReadableStatus } from '@/lib/helpers';
 import { SessionProps } from '@/lib/types';
 import { createDeliverable } from '@/services/deliverableService';
 import DeliverableForm from '../Form/DeliverableForm';
