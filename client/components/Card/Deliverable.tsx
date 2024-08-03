2855e35e0e3f8c5157320116c5079ea18d80f295
Robert Babaev <github@robertbabaev.tech>
2024-08-03 14:59:25 -0400

    fix styling on Deliverable Card

--- client/components/Card/Deliverable.tsx
+++ client/components/Card/Deliverable.tsx
@@ -6,6 +6,7 @@ import {
     ReadableStatus,
     classNames,
     determineGradeBGColour,
+    determineGradeTextColour,
 } from '@/lib/helpers';
 
 interface DeliverableCardProps extends Deliverable {
@@ -22,39 +23,41 @@ export default function DeliverableCard({
     handleEdit,
 }: DeliverableCardProps) {
     let bgColour = 'bg-primary-800';
+    let textColour = '';
     if (!goal || goal == 0 || status !== ItemStatus.COMPLETE) {
         //mostly so TypeScript doesn't freak out
     } else {
         bgColour = determineGradeBGColour(goal, mark);
+        textColour = determineGradeTextColour(goal, mark);
     }
     return (
         <div
             className={classNames(
                 bgColour,
-                'rounded-lg p-2 border-solid border-2 border-primary-500/10 font-bold'
+                'rounded-lg p-2 border-solid border-2 border-primary-500/10'
             )}
         >
             <div className="flex justify-between">
-                <div className="flex flex-col justify-between gap-2">
-                    <h3 className="text-lg">
+                <div className="flex flex-col justify-between basis-1/2">
+                    <h3 className="text-lg text-left font-bold">
                         {name} ({weight}%)
                     </h3>
-                    <h4>{ReadableStatus(status)}</h4>
+                    <div className="flex flex-col gap-4">
+                        <h4 className="font-bold basis-3/4">
+                            {(status === ItemStatus.COMPLETE && mark) || '--'} /{' '}
+                            {goal} %
+                        </h4>
+                        <h4 className="italic">{ReadableStatus(status)}</h4>
+                    </div>
                 </div>
 
-                <div className="flex flex-col justify-between gap-2 w-32">
-                    <h4 className="px-2 flex justify-between">
-                        <span>
-                            {(status === ItemStatus.COMPLETE && mark) || '--'}%
-                        </span>
-                        <span>/</span>
-                        <span>{goal}%</span>
-                    </h4>
+                <div className="flex flex-col justify-center gap-2 w-32">
                     <Button
                         endContent={
                             <PencilIcon className="h-6 w-6"></PencilIcon>
                         }
                         onPressEnd={handleEdit}
+                        className="top-1"
                     >
                         Edit
                     </Button>
