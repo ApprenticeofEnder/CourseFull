9b91c1566d553d88a194f0458b73c11e11ff90ab
Robert Babaev <github@robertbabaev.tech>
2024-08-03 15:10:50 -0400

    remove useless imports + update modal title

--- client/components/Modal/UpdateDeliverable.tsx
+++ client/components/Modal/UpdateDeliverable.tsx
@@ -4,16 +4,10 @@ import {
     ModalHeader,
     ModalBody,
     ModalFooter,
-    Input,
-    Textarea,
 } from '@nextui-org/react';
-import { Listbox, Transition } from '@headlessui/react';
 
 import Button from '@/components/Button/Button';
-// import ConfirmButton from '@/components/Button/ConfirmButton';
-import DisclosureButton from '@/components/Button/DisclosureButton';
 import { ItemStatus } from '@/lib/enums';
-import { ReadableStatus } from '@/lib/helpers';
 import { Deliverable, SessionProps } from '@/lib/types';
 import { updateDeliverable } from '@/services/deliverableService';
 import DeliverableForm from '../Form/DeliverableForm';
@@ -69,7 +63,7 @@ export default function UpdateDeliverableModal({
                         Edit Deliverable
                     </ModalHeader>
                     <ModalBody>
-                        <h3>Goal: {deliverable.goal}</h3>
+                        <h3>Goal: {deliverable.goal}%</h3>
                         <DeliverableForm
                             name={name}
                             setName={setName}
