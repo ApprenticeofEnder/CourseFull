2c7a74600177b9a3c4aadb6bc5ef2707b0cf9b5d
Robert Babaev <github@robertbabaev.tech>
2024-09-16 23:49:37 -0400

    switch to production deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://dev.coursefull.app", "http://app:3000"
+    origins "http://localhost:3000", "http://app:3000", "https://coursefull.app"
 
     resource "*",
       headers: :any,
