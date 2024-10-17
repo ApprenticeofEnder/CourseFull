dab3c60379e869a8f0f06eb7c253ed5c1b7775c7
Robert Babaev <github@robertbabaev.tech>
2024-10-17 06:06:57 -0400

    add tmux init script

--- tmux-env.sh
+++ tmux-env.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+SESSION_NAME="coursefull"
+
+tmux has-session -t $SESSION_NAME 2>/dev/null
+
+if [ $? != 0 ]; then
+    tmux new-session -d -s $SESSION_NAME -n dev_server
+
+    tmux send-keys -t $SESSION_NAME:dev_server "just dev" C-m
+
+    tmux new-window -t $SESSION_NAME -n test_server
+    tmux new-window -t $SESSION_NAME -n test
+    tmux new-window -t $SESSION_NAME -n git
+
+    tmux set-option -t $SESSION_NAME status on
+
+    tmux select-window -t $SESSION_NAME:dev_server
+fi
+
+tmux attach-session -t $SESSION_NAME
