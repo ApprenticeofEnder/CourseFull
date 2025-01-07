db8cd26b9e372b5784bb95fb4227ea243e5f4a42
Robert Babaev <github@robertbabaev.tech>
2025-01-07 00:54:37 -0500

    add FAQ accordion

--- client/components/Home/Faq.tsx
+++ client/components/Home/Faq.tsx
@@ -1,3 +1,58 @@
+import { motion, type Variants } from 'motion/react';
+import { ReactNode, useState } from 'react';
+import {
+    ArrowTrendingUpIcon,
+    CalendarIcon,
+    EyeIcon,
+    InformationCircleIcon,
+} from '@heroicons/react/24/outline';
+
+import Button from '@components/Button/Button';
+import {
+    Accordion,
+    AccordionItem,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from '@nextui-org/react';
+import { useHomePage } from '@lib/home/HomePageContext';
+import { expositionVariants, cardVariants } from '@lib/animations/variants';
+
 export default function Faq() {
-    return <div></div>;
+    const { refs } = useHomePage();
+
+    const questions = [
+        {
+            question: 'How much does CourseFull cost?',
+            answer: "$5 per course. That's it! You get 3 courses for free, and you can keep the credits for as long as you have an account.",
+        },
+    ];
+
+    return (
+        <motion.div
+            ref={refs?.faqRef.ref}
+            className="flex flex-col justify-center items-center gap-4 sm:px-16 sm:py-8"
+        >
+            <motion.h2
+                initial="offscreen"
+                whileInView="onscreen"
+                variants={expositionVariants}
+            >
+                FAQ
+            </motion.h2>
+            <Accordion variant="splitted">
+                {questions.map(({ question, answer }, index) => {
+                    return (
+                        <AccordionItem
+                            key={index}
+                            aria-label={question}
+                            title={question}
+                        >
+                            {answer}
+                        </AccordionItem>
+                    );
+                })}
+            </Accordion>
+        </motion.div>
+    );
 }
