dca61b7c74a4e42a67fcc48722e6e92662dfcddc
Robert Babaev <github@robertbabaev.tech>
2025-01-07 15:56:06 -0500

    update mechanics info section

--- client/components/Home/MechanicsInfo.tsx
+++ client/components/Home/MechanicsInfo.tsx
@@ -1,6 +1,7 @@
 import { useHomePage } from '@lib/home/HomePageContext';
 import { expositionVariants, cardVariants } from '@lib/animations/variants';
 import { motion } from 'motion/react';
+import Section from '@components/Home/Section';
 
 interface Step {
     stepNumber: number;
@@ -21,26 +22,14 @@ export default function MechanicsInfo() {
     }));
 
     return (
-        <motion.div
-            className="flex flex-col justify-center items-center gap-4 sm:px-16 sm:py-8"
-            ref={refs?.mechanicsRef.ref}
-        >
-            <motion.h2
-                initial="offscreen"
-                whileInView="onscreen"
-                variants={expositionVariants}
-            >
-                Take charge of your academic goals!
-            </motion.h2>
+        <Section ref={refs?.mechanicsRef.ref} title="How It Works">
             <motion.p
                 initial="offscreen"
                 whileInView="onscreen"
                 variants={expositionVariants}
                 className="text-center"
             >
-                <b>Got a target to hit?</b> We do the math behind the scenes so
-                you always know what you need to get on your assignments, exams,
-                and other deliverables.
+                It's easier than you think.
             </motion.p>
             <div className="flex flex-col sm:flex-row justify-between gap-4">
                 {cardData.map(({ stepNumber, step }: Step, index: number) => (
@@ -61,6 +50,6 @@ export default function MechanicsInfo() {
                     </motion.div>
                 ))}
             </div>
-        </motion.div>
+        </Section>
     );
 }
